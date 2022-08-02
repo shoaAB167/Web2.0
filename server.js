80173da2b8bfc6986ed22c771b07c9e5d99b21bf
shoaAB167 <shaikhshoaib16799@gmail.com>
2022-08-02 12:59:55 +0530

    POST INPUT SETUP

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/', (req, res) => {
+  const email = req.body.email;
+  const amount = req.body.amount;
+
+  res.send({"amount" : amount, "email" : email});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
