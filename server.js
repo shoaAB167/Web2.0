7fc8e0942bc81db08fb2fa8c363e092260b7fe41
shoaAB167 <shaikhshoaib16799@gmail.com>
2022-08-03 18:30:02 +0530

    Creating payment with paypal

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const {save_user_information} = require('./model/server_db');
 const path = require('path');
 const publicPath = path.join(__dirname, './public');
-const paypal = require('paypal-rest-sdl');
+const paypal = require('paypal-rest-sdk');
 
 const port = 3000
 
@@ -31,7 +31,55 @@ app.post('/post_info',async  (req, res) => {
     return return_info;
   }
   var result = await save_user_information({"amount" : amount, "email" : email});
-  res.send(result);
+
+
+  //paypal
+  var create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://locahost:3000/success",
+        "cancel_url": "http://locahost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Lottery",
+                "sku": "Funding",
+                "price": amount,
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": amount
+        },
+        'payee' : {
+          'email' : 'lottery_app@app.com'
+        },
+        "description": "Lottery purchase"
+    }]
+
+};
+
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        console.log("Create Payment Response");
+        console.log(payment);
+        for(var i = 0; i< payment.links.length; i++){
+          if(payment.links[i].rel =='approval_url'){
+            return res.send(payment.links[i].href);
+          }
+        }
+    }
+});
+//res.send(result);
 });
 
 app.get('/get_info',async (req, res) => {
