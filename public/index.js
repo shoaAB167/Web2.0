900c00caeb727da815f620c7259d89bcea4411ad
shoaAB167 <shaikhshoaib16799@gmail.com>
2022-08-04 14:04:07 +0530

    touchup with bootstrap

--- public/index.js
+++ public/index.js
@@ -3,15 +3,15 @@ class App extends React.Component{
     super(props);
     this.state = {
       'total_amount' : 1000,
-      'amount': 100,
-      'email':'' ,
+      'amount' : 100,
+      'email' : '',
     }
   }
   async componentDidMount(){
     const result = await axios.get('/get_total_amount');
     this.setState({total_amount : result.data["0"].total_amount});
   }
-  onSubmit = async (event) => {
+  onSubmit = async (event) =>{
     event.preventDefault();
     const response = await axios.post('/post_info',{
       amount : this.state.amount,
@@ -21,23 +21,46 @@ class App extends React.Component{
   }
   render(){
     return(
-    <div>
-        <h1>Lottery Application 2.0 </h1>
-      <div>
-      <p> Total lottery amount is : {this.state.total_amount >0 ? this.state.total_amount : 0}</p>
-      </div>
-      <form onSubmit={this.onSubmit}>
-      <input placeholder="amount" value = {this.state.amount}
-      onChange = {event=> this.setState({amount :event.target.value})}/>
-      <input placeholder="email" value = {this.state.email}
-      onChange = {event=> this.setState({email : event.target.value})}/>
-      <button type="submit"> participate </button>
-      </form>
-    </div>
+      <div className="container">
+            <div className="col-md-12">
+              <div className="card text-center">
+                  <div className="card-header">
+                    <h1>TOTAL WINNING POOL IS </h1>
+                    <div className ="block">
+                        <div className="circle">
+                          <p>${this.state.total_amount}</p>
+                        </div>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                  <form onSubmit={this.onSubmit}>
+                      <div className="form-group">
+                        <label >Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email"
+                          onChange = {event=> this.setState({email :event.target.value})}
+                        />
+                        <small className="form-text text-muted">Well never share your email with anyone else.</small>
+                      </div>
+                      <div className="form-group">
+                        <label>Amount</label>
+                        <input type="number" className="form-control"  placeholder="Enter Amount"
+                          value={this.state.amount}
+                          onChange={event => this.setState({amount : event.target.value})}
+                        />
+                        <small className="form-text text-muted">Enter the amount you would like to participate with</small>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+              </div>
+            </div>
+        </div>
     )
   }
 };
 
-ReactDOM.render(<div><App /></div>,
-  document.getElementById('reactBinding')
-);
+ReactDOM.render(
+  <div>
+    <App />
+  </div>
+, document.getElementById('reactBinding'));
